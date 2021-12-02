e1f986de54d8bdb1a9a1477d59984e369802624d
fange123 <2030564240@qq.com>
2021-12-02 15:25:39 +0800

    feat:响应式对象的新花样

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,52 @@
   <div id="id">
       <h1>{{count}}</h1>
       <h1>{{double}}</h1>
+      <ul>
+        <li v-for="num in number" :key="num"><h1>{{ num }}
+</h1></li>
+      </ul>
+      <h1>{{person.name}}</h1>
       <button @click="increment">添加👍</button>
   </div>
 </template>
 
 <script lang="ts">
-import {ref,computed} from 'vue'
+import {computed,reactive,toRefs} from 'vue'
+interface IDateProps {
+  count: number
+  double: number
+  increment(): void
+  number: number[]
+  person:{name?:string}
+
+}
 
 export default {
   name: 'App',
   setup(){
-    const count = ref(0)
-    //*computed的参数是一个函数
-    const double = computed(()=> count.value*2)
-    const increment = ()=> {
-      count.value++
-    }
-    return {count,increment,double
+    // const count = ref(0)
+    // //*computed的参数是一个函数
+    // const double = computed(()=> count.value*2)
+    // const increment = ()=> {
+    //   count.value++
+    // }
+
+    //*reactive是一个函数，接受一个对象，可以把分散的变量方法都合并在一个对象里面
+    const data:IDateProps = reactive({
+      count: 0,
+      increment: ()=>data.count++,
+      double: computed(()=>data.count*2),
+      number:[1,2,3,4],
+      person:{}
+    })
+    //*不能直接这样返回，因为这样返回出来的是类型而不是值，可以用toRefs
+    // return {...data}
+    //? vue3中可以直接修改属性的值
+    data.number[2] = 1000
+    data.person.name = "John"
+
+    const refData = toRefs(data)
+    return {...refData
 }
 
   }
@@ -27,11 +56,14 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
+li {
+  list-style: none;
+}
+
+
 </style>
