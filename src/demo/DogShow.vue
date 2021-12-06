e22d62f1ade897a661dedadf9dba8e354dd5eef0
fange123 <2030564240@qq.com>
2021-12-06 15:44:47 +0800

    feat:onErrorCaptured处理suspense里面的错误

---
+++ src/demo/DogShow.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="">
+<img :src="data?.message" alt="">
+
+</div>
+</template>
+
+<script lang='ts'>
+import {defineComponent} from 'vue'
+import axios from 'axios'
+
+
+export default defineComponent({
+    name:'DogShow',
+    async setup() {
+
+      const result = await axios.get('https://dog.ceo/api/breeds/image/random')
+       return { data:result.data}
+    },
+})
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+
+</style>
--- src/demo/Ts.vue
+++ src/demo/Ts.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="ts">
     <h1>我是ts</h1>
+    <p>{{error}}</p>
 
     <Modal :isOpen="modalIsOpen" @close-modal="closeModal">
       <!-- //~插槽可以获取到父组件里的内容 -->
@@ -21,6 +22,17 @@
 
       </template>
     </Suspense>
+    <Suspense>
+    <template #default>
+      <DogShow />
+
+    </template>
+    <template #fallback>
+      <h1>loading</h1>
+    </template>
+
+  </Suspense>
+
 
 
 
@@ -31,15 +43,18 @@
 <script lang="ts">
 import Modal from "../components/Modal.vue";
 import SuspenseDemo from './SuspenseDemo.vue'
+import DogShow from "./DogShow.vue"
 
-import { ref, Suspense } from 'vue';
+import { ref,onErrorCaptured } from 'vue';
 
 
 export default {
   name: "Ts",
   components: {
     Modal,
-    SuspenseDemo
+    SuspenseDemo,
+    DogShow
+
 
   },
   setup() {
@@ -53,12 +68,24 @@ export default {
       modalIsOpen.value = false
 
     }
+    const error = ref(null)
+    onErrorCaptured((e:any)=>{
+      error.value = e
+
+      //~onErrorCaptured这个钩子函数返回一个boolean值，表示是否向上传播
+
+      return true
+
+    })
+
 
     return {
       name,
       modalIsOpen,
       modalOpen,
-      closeModal
+      closeModal,
+      error
+
 
     };
   },
